c85867e7baae93cccf602a121233384a659fa244
Maria <marfayaga@gmail.com>
2023-06-26 16:57:35 +0300

    Заняття 9

--- index.js
+++ index.js
@@ -1,161 +1,120 @@
-//"use strict";
-// g: undefined
-g = 10
-// g: 10
-j = 20 // помилка
+'use strict';
 
-123 //числовий літерал
-'123' // рядковий літерал
-"123"; // рядковий літерал
-`123 ${j} ${(123*456-71)}` // `123 20 56017`
-true // булевий літерал
-null
-undefined
+/*const x = +prompt('Enter x')
+const y = +prompt('Enter y')
+const z = +prompt('Enter z')
+const t = +prompt('Enter t')
+const k = +prompt('Enter k')
 
-const h = null
+// x*y + z*t, xє(0;1), yє(5;10), zє(-oo;oo), tє(-5;5)
 
-'I\'m 100'
-
-+'123' // 123
-+'ierywiu' // NaN
-+123 // 123
-+true // 1
-+false // 0
-+null // 0
-+undefined // NaN
--'123' // -123
--'kjhkj' // NaN
--123 // -123
--true // -1
--false // 0
--null // 0
--undefined // NaN
-
-!'skdfhjkhf' // false
-!123 // false
-
-!true // false
-!false // true
-
-!null // true
-!undefined // true
-
-!!'fhksjhf' // true
-
-Boolean('ksdkfh') // true
-Number(true) // 1
-String(false) // 'false'
-
-12 + 3 // 15
-
-'' + 10 // '10'
-'tyru' + true // 'tyrutrue'
-'u ' + null // ' null'
-
-null + true // 1
-+'wurtweuyr' + true // NaN
-
-'tyru' - true // NaN
-
-123*0 // 0
-
-45 / 0 // Infinity
--123 / 0 // -Infinity
-
-0 / 0 // NaN
-
-true || false // true
-true && false // false
-
-true || false && true // true
-(true || false) && true // true
-
-/*djsfkjhf
-dlfkdskfj
-sdklkj
-
-sfkldkjfhd*/
-
-function a() {
-  console.log('a')
-  return true
-}
-
-function b() {
-  console.log('b')
-  return false
-}
-
-function c() {
-  console.log('c')
-  return true
-}
-
-a() || b() && c()
-
-var myVar = 123
-
-let sum = myVar + 78
-
-let myVar2 = 456
-
-myVar2 = 18 // помилка
-
-myVar = 30 // помилки не буде
-
-var g = 123
-// g: 123
-
-let i = 0;
-
-// actions
-
-i = i + 1
-i += 1
-i++
-++i
-// i: 4
-const y = i++ // y: 4
-// i: 5
-const x = ++i // x: 5
-
---i // i = i - 1
-i-- // i = i - 1
-
-i = i * 5
-i *= 5
-
-i /= 10
-
-const result = alert('Hello!')
-console.log(result) // undefined
-
-const input = +prompt('How old are you?')
-console.log(input)
-
-if (input) {
-  // non zero number, non NaN
-}
-
-const yesOrNo = confirm('Так чи ні?')
-console.log(yesOrNo)
-
-if (yesOrNo) {
-  alert('ви погодилися')
-}
-else {
-  alert('ви не погодилися')
-}
-
-const appleCount = +prompt('How many apples?')
-
-if (appleCount === 1) {
-  alert('1 apple')
-}
-else if (appleCount > 1 || appleCount === 0) {
-  alert(appleCount + ' apples')
+if (!isNaN(x) && 0 < x && x < 1 && 
+  !isNaN(y) && 5 < y && y < 10 &&
+  !isNaN(z) &&
+  !isNaN(t) && -5 < t && t < 5 &&
+  !isNaN(k) && 10 < k && k < 20
+) {
+  const result = x * y + z * t + k
+  alert('x * y + z = ' + result)
 }
 else {
   alert('Error')
 }
 
+if (!isNaN(x) && 0 < x && x < 1) {
+  if (!isNaN(y) && 5 < y && y < 10) {
+    if (!isNaN(z)) {
+      if (!isNaN(t) && -5 < t && t < 5) {
+        if (!isNaN(k) && 10 < k && k < 20) {
+          const result = x * y + z * t
+          alert('x * y + z = ' + result)
+        }
+        else {
+          alert('Invalid k')
+        }
+      }
+      else {
+        alert('Invalid t')
+      }
+    }
+    else {
+      alert('Invalid z')
+    }
+  }
+  else {
+    alert('Invalid y')
+  }
+}
+else {
+  alert('Invalid x')
+}
+
+
+function getNumber(name, min = Number.NEGATIVE_INFINITY, max = Number.POSITIVE_INFINITY) {
+  const num = +prompt('Enter ' + name)
+  if (isNaN(num)) {
+    alert('Invalid ' + name)
+    return
+  }
+
+  if (num <= min) {
+    console.log('Set to min')
+    return min
+  }
+
+  if (num >= max) {
+    console.log('Set to max')
+    return max
+  }
+  
+  return num
+}
+
+{
+  const x = getNumber('x', 0, 1)
+  const y = getNumber('y', 5, 10)
+  const z = getNumber('z')
+  const t = getNumber('t', -5, 5)
+  const k = getNumber('k', 10, 20)
+
+  if (x !== undefined && y !== undefined && z !== undefined && t !== undefined && k !== undefined) {
+    const result = x * y + z * t + k
+    alert('x * y + z = ' + result)
+  }
+}*/
+
+function isValid(base, degree) {
+  return !isNaN(base) && !isNaN(degree) && Number.isInteger(+degree)
+}
+
+function pow(base, degree) {
+  if (degree < 0) {
+    return 1 / pow(base, -degree)
+  }
+
+  if (degree === 0) {
+    return 1
+  }
+
+  return base * pow(base, degree - 1)  
+}
+
+const base = prompt('Enter base') || ''
+const degree = prompt('Enter degree')
+
+if (!isValid(base, degree)) {
+  alert('Error')
+}
+else {
+  const result = pow(+base, +degree)
+  alert('base^degree = ' + result)
+}
+
+const lis = [...document.querySelectorAll('li')]
+function onClick(event) {
+  alert('First')
+}
+
+lis.forEach(li => li.addEventListener('click', onClick))
+
+//lis.forEach(li => li.removeEventListener('click', onClick))
