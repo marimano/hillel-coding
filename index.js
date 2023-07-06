dcdb19015f715874f990c6e8b29b537b02c44798
Maria <marfayaga@gmail.com>
2023-07-06 17:14:44 +0300

    Заняття 12

--- index.js
+++ index.js
@@ -1,87 +1,8 @@
 'use strict';
 
-/*const x = +prompt('Enter x')
-const y = +prompt('Enter y')
-const z = +prompt('Enter z')
-const t = +prompt('Enter t')
-const k = +prompt('Enter k')
-
-// x*y + z*t, xє(0;1), yє(5;10), zє(-oo;oo), tє(-5;5)
-
-if (!isNaN(x) && 0 < x && x < 1 && 
-  !isNaN(y) && 5 < y && y < 10 &&
-  !isNaN(z) &&
-  !isNaN(t) && -5 < t && t < 5 &&
-  !isNaN(k) && 10 < k && k < 20
-) {
-  const result = x * y + z * t + k
-  alert('x * y + z = ' + result)
-}
-else {
-  alert('Error')
-}
-
-if (!isNaN(x) && 0 < x && x < 1) {
-  if (!isNaN(y) && 5 < y && y < 10) {
-    if (!isNaN(z)) {
-      if (!isNaN(t) && -5 < t && t < 5) {
-        if (!isNaN(k) && 10 < k && k < 20) {
-          const result = x * y + z * t
-          alert('x * y + z = ' + result)
-        }
-        else {
-          alert('Invalid k')
-        }
-      }
-      else {
-        alert('Invalid t')
-      }
-    }
-    else {
-      alert('Invalid z')
-    }
-  }
-  else {
-    alert('Invalid y')
-  }
-}
-else {
-  alert('Invalid x')
-}
-
-
-function getNumber(name, min = Number.NEGATIVE_INFINITY, max = Number.POSITIVE_INFINITY) {
-  const num = +prompt('Enter ' + name)
-  if (isNaN(num)) {
-    alert('Invalid ' + name)
-    return
-  }
-
-  if (num <= min) {
-    console.log('Set to min')
-    return min
-  }
-
-  if (num >= max) {
-    console.log('Set to max')
-    return max
-  }
-  
-  return num
-}
-
-{
-  const x = getNumber('x', 0, 1)
-  const y = getNumber('y', 5, 10)
-  const z = getNumber('z')
-  const t = getNumber('t', -5, 5)
-  const k = getNumber('k', 10, 20)
-
-  if (x !== undefined && y !== undefined && z !== undefined && t !== undefined && k !== undefined) {
-    const result = x * y + z * t + k
-    alert('x * y + z = ' + result)
-  }
-}*/
+window.addEventListener('error', (event) => {
+  console.log('Ooops', event)
+})
 
 function isValid(base, degree) {
   return !isNaN(base) && !isNaN(degree) && Number.isInteger(+degree)
@@ -99,7 +20,7 @@ function pow(base, degree) {
   return base * pow(base, degree - 1)  
 }
 
-const base = prompt('Enter base') || ''
+/*const base = prompt('Enter base') || ''
 const degree = prompt('Enter degree')
 
 if (!isValid(base, degree)) {
@@ -108,13 +29,347 @@ if (!isValid(base, degree)) {
 else {
   const result = pow(+base, +degree)
   alert('base^degree = ' + result)
+}*/
+
+const categories = ['A', 'B', 'C']
+
+const items = [
+  {
+    name: 'First',
+    value: 1,
+    category: 'A'
+  },
+  {
+    name: 'Second',
+    value: 2,
+    category: 'A'
+  },
+  {
+    name: 'Third',
+    value: 3,
+    category: 'B'
+  },
+  {
+    name: 'Fourth',
+    value: 4,
+    category: 'B'
+  }
+]
+
+const createDropdown = (function() {
+  function createEmptyDD() {
+    const dd = document.createElement('div')
+    dd.classList.add('custom-dd')
+    dd.innerHTML = `<div class="current-element">
+        <span class="current-element-value"></span>
+        <button>\/</button>
+      </div>
+      <ul class="list">
+      </ul>`
+    return dd
+  }
+  
+  function initDD(dd, setCurrentItemIndex) {
+    const list = dd.querySelector('.list')
+  
+    const currentElementBox = dd.querySelector('.current-element')
+    currentElementBox.addEventListener('click', () => {
+      list.classList.toggle('open')
+    })
+  
+  
+    window.addEventListener('click', event => {
+      if (!currentElementBox.contains(event.target)) {
+        list.classList.remove('open')
+      }
+    })
+  
+    const currentElement = dd.querySelector('.current-element-value')
+    list.addEventListener('click', event => {
+      if (event.target.nodeName === 'LI') {
+        currentElement.innerText = event.target.innerText
+        setCurrentItemIndex(+event.target.dataset.index)
+      }
+    })
+  }
+  
+  function fillItems(categories, items, dd, currentItemIndex, onItemElementCreated, isEmptyCategoriesShown) {
+    const list = dd.querySelector('.list')
+    categories.forEach(category => {
+      const categoryItems = items.filter(item => item.category === category)
+      if (categoryItems.length || isEmptyCategoriesShown) {
+        const categoryEl = document.createElement('div')
+        categoryEl.innerText = category
+        list.append(categoryEl)
+      }
+
+      categoryItems.forEach((item, index) => {
+        const li = document.createElement('li')
+        li.innerText = item.name
+        li.dataset.index = index
+        onItemElementCreated(li, index)
+        list.append(li)
+      })
+    })
+    
+  
+    const currentElement = dd.querySelector('.current-element-value')
+    currentElement.innerText = items[currentItemIndex].name
+  } 
+  
+  return function createDropdown(categories, items, parentElement, onItemElementCreated, isEmptyCategoriesShown) {
+    let currentItemIndex = 0
+    const dd = createEmptyDD()
+    parentElement.append(dd)
+  
+    initDD(dd, newIndex => currentItemIndex = newIndex)
+    fillItems(categories, items, dd, currentItemIndex, onItemElementCreated, isEmptyCategoriesShown)
+  }
+})()
+
+createDropdown(categories, items, document.body, decorateElement, false)
+
+function decorateElement(element, index) {
+  if (index === 3) {
+    element.classList.add('third-element')
+  }
 }
 
-const lis = [...document.querySelectorAll('li')]
-function onClick(event) {
-  alert('First')
+const purchasings = []
+
+function onFormSubmit(event) {
+  event.preventDefault()
+  const { elements } = event.target
+  const name = elements.name.value.trim()
+  const amount = +elements.amount.value
+
+  const errorElement = event.target.querySelector('.error')
+  if (!name) {
+    errorElement.innerText = 'Name cannot be empty'
+    return
+  }
+
+  if (purchasings.some(p => p.name === name)) {
+    errorElement.innerText = 'Such purchasing is already added'
+    return
+  }
+
+  if (amount < 1 || amount > 1000000) {
+    errorElement.innerText = 'Amount should be within 1 and 1000000'
+    return
+  }
+
+  errorElement.innerText = ''
+  addPurchasing(name, amount)
 }
 
-lis.forEach(li => li.addEventListener('click', onClick))
+function addPurchasing(name, amount) {
+  purchasings.push({
+    name,
+    amount
+  })
+  const shoppingListEl = document.querySelector('.shopping-list')
+  const pElement = document.createElement('li')
+  pElement.innerHTML = `<span>${name}: ${amount}</span><button>X</button>`
+  pElement.querySelector('button').onclick = () => {
+    const msg = 'Are you sure you want to delete ' + name + ' from the list?'
+    document.querySelector('.modal-dialog-text').innerText = msg
+    document.querySelector('.modal-bg').style.display = ''
 
-//lis.forEach(li => li.removeEventListener('click', onClick))
+    document.querySelector('.yes-btn').onclick = () => {
+      const itemIndexToDelete = purchasings.findIndex(p => p.name === name)
+      purchasings.splice(itemIndexToDelete, 1)
+      shoppingListEl.children[itemIndexToDelete].remove()
+
+      document.querySelector('.modal-bg').style.display = 'none'
+    }    
+  }
+  shoppingListEl.append(pElement)
+}
+
+document.querySelector('.no-btn').onclick = () => {
+  document.querySelector('.modal-bg').style.display = 'none'
+}
+
+document.querySelector('.modal-bg').onclick = event => {
+  if (event.target.classList.contains('modal-bg')) {
+    event.target.style.display = 'none'
+  }
+}
+
+function showInfo() {
+  alert('User info: ' + this.name + ', ' + this.age)
+}
+
+const obj = {
+  name: 'Joe',
+  age: 20,
+  showInfo,
+  get info() {
+    return this.name + ', ' + this.age + (this.additionalInfo ? ` (${this.additionalInfo})` : '')
+  },
+  set info(value) {
+    if (value?.length > 5) {
+      this.additionalInfo = value
+    }
+  }
+}
+
+console.log(obj.info) // Joe, 20
+
+obj.info = '123' // error
+console.log(obj.info) // Joe, 20
+obj.info = 'cool guy'
+console.log(obj.info) // Joe, 20 (cool guy)
+
+const obj2 = {
+  name: 'Jane',
+  age: 25,
+  showInfo
+}
+
+const obj3 = {
+  name: 'Joe',
+  age: 20,
+  showInfo
+}
+
+const baseObj = {
+  showInfo
+}
+const objWithProto = Object.create(baseObj)
+objWithProto.name = 'Proto'
+objWithProto.age = 30
+
+function User(name, age, surname) {
+  this.name = name
+  this.surname = surname
+  this.age = age
+  User.count++;
+}
+
+//User.prototype = Object.create(User.prototype)
+User.prototype.showInfo = function () {
+  alert('User info: ' + this.getInfo())
+}
+User.prototype.getInfo = function() {
+  return this.name + ' ' + this.surname + ', ' + this.age
+}
+Object.defineProperty(User.prototype, 'info', {
+  get: function() {
+    return this.name + ', ' + this.age + (this.additionalInfo ? ` (${this.additionalInfo})` : '')
+  },
+  set: function(value) {
+    if (value?.length > 5) {
+      this.additionalInfo = value
+    }
+  }
+});
+User.count = 0
+
+function AdminUser(name, age, surname, permissions) {
+  User.call(this, name, age, surname)
+  this.permissions = permissions
+}
+
+AdminUser.prototype = Object.create(User.prototype)
+AdminUser.prototype.accessRoot = function() {
+  // access secret data
+}
+
+function GuestUser(name, age, surname, lastVisit) {
+  User.call(this, name, age, surname)
+  this.lastVisit = lastVisit
+}
+
+GuestUser.prototype = Object.create(User.prototype)
+GuestUser.prototype.upgradeToUser = function() {
+  // upgrade
+}
+
+function ConsoleUser(name, age, surname) {
+  User.call(this, name, age, surname)
+}
+
+ConsoleUser.prototype = Object.create(User.prototype)
+ConsoleUser.prototype.showInfo = function () {
+  console.log('User info: ' + this.getInfo())
+}
+
+const user1 = new User('User', 40)
+user1.age = 45
+
+const admin = new AdminUser('Tim', 50)
+const guest = new GuestUser('Incognito', 25)
+const consoleUser = new ConsoleUser('Dig', 70, 'Cool')
+
+const users = [user1, admin, guest, consoleUser]
+console.log('There are ' + User.count + ' users')
+
+
+class NewUser {
+  static count = 0
+  constructor(name, age, surname) {
+    this.name = name
+    this.age = age
+    this.surname = surname
+    NewUser.count++
+  }
+
+  showInfo() {
+    alert('User info: ' + this.getInfo())
+  }
+
+  getInfo() {
+    return this.name + ' ' + this.surname + ', ' + this.age
+  }
+
+  get info() {
+    return this.name + ', ' + this.age + (this.additionalInfo ? ` (${this.additionalInfo})` : '')
+  }
+
+  set info(value) {
+    if (value?.length > 5) {
+      this.additionalInfo = value
+    }
+  }
+}
+
+class NewAdminUser extends NewUser {
+  constructor(name, age, surname, permissions) {
+    super(name, age, surname)
+    this.permissions = permissions
+  }
+
+  accessRoot() {
+    // access secret data
+  }
+}
+
+class NewGuestUser extends NewUser {
+  constructor(name, age, surname, lastVisit) {
+    super(name, age, surname)
+    this.lastVisit = lastVisit
+  }
+
+  upgradeToUser() {
+    // upgrade
+  }
+}
+
+class NewConsoleUser extends NewUser {
+  showInfo() {
+    console.log('User info: ' + this.getInfo())
+  }
+}
+
+const newuser1 = new NewUser('User', 40)
+
+const newadmin = new NewAdminUser('Tim', 50)
+const newguest = new NewGuestUser('Incognito', 25)
+const newconsoleUser = new NewConsoleUser('Dig', 70, 'Cool')
+console.log('There are ' + NewUser.count + ' users')
+
+
+
+
